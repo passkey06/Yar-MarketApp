ea2610a1010307f877e0c6ce319d7b8bbec27bcd
passkey06 <passkey1@gmail.com>
2026-07-12 14:59:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const categories = document.querySelectorAll(".category");
 
+categories.forEach(card=>{
+card.addEventListener("click",()=>{
+
+alert(card.innerText+" kategorisi yakında aktif olacak.");
+
+});
+});
