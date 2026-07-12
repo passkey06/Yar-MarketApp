ffe5f5a9fff3dc04f1069feb66fab9342e0521dd
passkey06 <passkey1@gmail.com>
2026-07-12 15:02:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
-const categories = document.querySelectorAll(".category");
+const products = {
+  "Temel Gıda": [
+    "Unlu Mamuller",
+    "Bakliyat",
+    "Makarna",
+    "Sıvı Yağlar"
+  ],
+  "Manav": [
+    "Domates",
+    "Patates",
+    "Soğan",
+    "Muz",
+    "Şeftali",
+    "Biber"
+  ],
+  "Şarküteri": [
+    "Tavuk",
+    "Kırmızı Et"
+  ],
+  "İçecekler": [
+    "Su",
+    "Maden Suyu",
+    "Kola",
+    "Soğuk Çay",
+    "Süt"
+  ],
+  "Atıştırmalık": [
+    "Ülker",
+    "Eti"
+  ],
+  "Temizlik": [
+    "Çamaşır Deterjanı",
+    "Bulaşık Deterjanı"
+  ]
+};
 
-categories.forEach(card=>{
-card.addEventListener("click",()=>{
+const cards = document.querySelectorAll(".category");
+const area = document.getElementById("products");
 
-alert(card.innerText+" kategorisi yakında aktif olacak.");
+cards.forEach(card => {
+
+card.onclick = () => {
+
+const category = card.querySelector("h3").innerText;
+
+let html = `<h2>${category}</h2>`;
+
+products[category].forEach(item => {
+
+html += `
+<div class="product">
+${item}
+</div>
+`;
 
 });
+
+area.innerHTML = html;
+
+window.scrollTo({
+top:document.body.scrollHeight,
+behavior:"smooth"
+});
+
+};
+
 });
