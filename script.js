f274b466de81963d25cb5abbe55bb1fdf5205759
passkey06 <passkey1@gmail.com>
2026-07-12 15:29:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,8 @@
-// Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-// Firebase Ayarları
 const firebaseConfig = {
-  apiKey: "BURAYA_API_KEY",
-  authDomain: "BURAYA_AUTH_DOMAIN",
-  projectId: "BURAYA_PROJECT_ID",
-  storageBucket: "BURAYA_STORAGE_BUCKET",
-  messagingSenderId: "BURAYA_MESSAGING_SENDER_ID",
-  appId: "BURAYA_APP_ID"
+  apiKey: "AIzaSyClZ4vbfNnWPQi6uTwbJH_6d1TRM_OoBqE",
+  authDomain: "deneme-a4c1e.firebaseapp.com",
+  projectId: "deneme-a4c1e",
+  storageBucket: "deneme-a4c1e.firebasestorage.app",
+  messagingSenderId: "582311785485",
+  appId: "1:582311785485:web:42c688aa9ca5de4bead144"
 };
-
-// Firebase Başlat
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Ürünleri Yükle
-async function urunleriYukle() {
-  const urunAlani = document.getElementById("urunler");
-
-  urunAlani.innerHTML = "<p>Ürünler yükleniyor...</p>";
-
-  try {
-    const snapshot = await getDocs(collection(db, "urunler"));
-
-    urunAlani.innerHTML = "";
-
-    snapshot.forEach((doc) => {
-      const urun = doc.data();
-
-      urunAlani.innerHTML += `
-        <div class="urun-kart">
-          <img src="${urun.resim}" alt="${urun.ad}">
-          <h3>${urun.ad}</h3>
-          <p>${urun.fiyat} ₺</p>
-          <button onclick="sepeteEkle('${doc.id}')">
-            Sepete Ekle
-          </button>
-        </div>
-      `;
-    });
-
-  } catch (err) {
-    urunAlani.innerHTML = "<p>Ürünler yüklenemedi.</p>";
-    console.error(err);
-  }
-}
-
-// Sepet
-window.sepeteEkle = function(id) {
-  alert(id + " sepete eklendi.");
-};
-
-// Sayfa Açılınca
-urunleriYukle();
